c2baa80e5b0bb5959eea45795ff5626749aad2d0
Matilda Mared <matildamared@live.se>
2022-04-22 08:12:51 +0200

    add basic category structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: "Hats",
+		},
+		{
+			id: 1,
+			title: "Jackets",
+		},
+		{
+			id: 1,
+			title: "Sneakers",
+		},
+		{
+			id: 1,
+			title: "Women",
+		},
+		{
+			id: 1,
+			title: "Men",
+		},
+	];
+
 	return (
-		<div>
-			<h1>Hello! 👋</h1>
-		</div>
+		<section className="categories-container">
+			{categories.map(({ id, title }) => (
+				<article className="category-container" key={id}>
+					<div className="background-image" />
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</article>
+			))}
+		</section>
 	);
 };
 
