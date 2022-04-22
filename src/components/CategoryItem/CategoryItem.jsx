4dc04b4c113d45b081e22926116b36a0b08fb141
Matilda Mared <matildamared@live.se>
2022-04-22 18:15:15 +0200

    add CategoryItem component

---
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./category-item.styles.scss";
+
+function CategoryItem({ category }) {
+	const { imageUrl, title } = category;
+
+	return (
+		<li className="category-container">
+			<div
+				className="background-image"
+				style={{ backgroundImage: `url(${imageUrl})` }}
+			/>
+			<div className="category-body-container">
+				<h2>{title}</h2>
+				<p>Shop Now</p>
+			</div>
+		</li>
+	);
+}
+
+export default CategoryItem;
