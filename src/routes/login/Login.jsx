a83a25eb079b3ef85a7e6175d19ea27698374d6f
Matilda Mared <matildamared@live.se>
2022-04-25 07:38:05 +0200

    add basic firebase authentication

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 function Login() {
+	const loginGoogleUser = async () => {
+		const response = await signInWithGooglePopup();
+		console.log(response);
+	};
+
 	return (
 		<div>
 			<h1>Login</h1>
+			<button onClick={loginGoogleUser}>Log in with Google Popup</button>
 		</div>
 	);
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCZU8q4m3QBDzM6Y5gvRh4o6aJucPFqWxw",
+	authDomain: "crown-clothing-db-faaab.firebaseapp.com",
+	projectId: "crown-clothing-db-faaab",
+	storageBucket: "crown-clothing-db-faaab.appspot.com",
+	messagingSenderId: "372126053688",
+	appId: "1:372126053688:web:1e13352f6fd13a06a4bdcd",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
