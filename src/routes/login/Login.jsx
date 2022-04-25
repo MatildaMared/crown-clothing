53f8c6023c052f64ae60051c712f67d8529bfd48
Matilda Mared <matildamared@live.se>
2022-04-25 07:59:48 +0200

    add basic connection to firestore database

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 function Login() {
 	const loginGoogleUser = async () => {
 		const response = await signInWithGooglePopup();
 		console.log(response);
+		createUserDocumentFromAuth(response.user);
 	};
 
 	return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
 	signInWithPopup,
 	GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCZU8q4m3QBDzM6Y5gvRh4o6aJucPFqWxw",
@@ -25,3 +26,14 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+};
