465e8f41ad1d0f73bea0fdcf32081d158f56426d
Matilda Mared <matildamared@live.se>
2022-04-29 07:34:00 +0200

    add product card component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ProductCard.styles.scss";
+import Button from "../Button/Button";
+
+const ProductCard = ({ product }) => {
+	const { name, price, imageUrl } = product;
+	return (
+		<article className="product-card-container">
+			<img src={imageUrl} alt={name} />
+			<div className="footer">
+				<span className="name">{name}</span>
+				<span className="price">{`${price} $`}</span>
+			</div>
+			<Button buttonType="inverted">Add to cart</Button>
+		</article>
+	);
+};
+
+export default ProductCard;
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { ProductsContext } from "../../contexts/ProductsContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import "./Shop.styles.scss";
 
 const Shop = () => {
 	const { products } = useContext(ProductsContext);
 	return (
-		<div>
-			{products.map(({ id, name }) => (
-				<article key={id}>
-					<h1>{name}</h1>
-				</article>
+		<section className="products-container">
+			{products.map((product) => (
+				<ProductCard product={product} key={product.id} />
 			))}
-		</div>
+		</section>
 	);
 };
 
