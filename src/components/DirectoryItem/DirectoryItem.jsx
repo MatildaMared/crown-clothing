ef0a6e5498ffc3afb994cc11f39ae1c4b52f494d
Matilda Mared <matildamared@live.se>
2022-05-01 17:12:23 +0200

    add links to different category pages on home page

--- src/components/DirectoryItem/DirectoryItem.jsx
+++ src/components/DirectoryItem/DirectoryItem.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 import {
 	DirectoryContainer,
@@ -6,10 +7,13 @@ import {
 } from "./DirectoryItem.styles";
 
 function DirectoryItem({ category }) {
-	const { imageUrl, title } = category;
+	const { imageUrl, title, route } = category;
+	const navigate = useNavigate();
+
+	const onNavigateHandler = () => navigate(route);
 
 	return (
-		<DirectoryContainer>
+		<DirectoryContainer onClick={onNavigateHandler}>
 			<BackgroundImage imageUrl={imageUrl} />
 			<DirectoryItemBody>
 				<h2>{title}</h2>
