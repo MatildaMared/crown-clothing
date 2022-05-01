176ab93002f08ae17f561ffd21cea56d85a6d083
Matilda Mared <matildamared@live.se>
2022-05-01 09:32:13 +0200

    add category preview component

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,23 +1,23 @@
 import React, { useContext } from "react";
 import { CategoriesContext } from "../../contexts/CategoriesContext";
-import ProductCard from "../../components/ProductCard/ProductCard";
+import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
 import "./Shop.styles.scss";
 
 const Shop = () => {
 	const { categoriesMap } = useContext(CategoriesContext);
 	return (
-		<>
-			{Object.keys(categoriesMap).map((title) => (
-				<div key={title}>
-					<h2>{title}</h2>
-					<section className="products-container">
-						{categoriesMap[title].map((product) => (
-							<ProductCard product={product} key={product.id} />
-						))}
-					</section>
-				</div>
-			))}
-		</>
+		<div className="shop-container">
+			{Object.keys(categoriesMap).map((title) => {
+				const products = categoriesMap[title];
+				return (
+					<CategoryPreview
+						key={title}
+						title={title}
+						products={products}
+					/>
+				);
+			})}
+		</div>
 	);
 };
 
